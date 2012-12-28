ba172e910d317dc5c1d6e69cc25a88472d94592e
nebkat <nebkat@teamhacksung.org>
2012-12-28 10:40:45 +0000

    cout: move to $out
    Change-Id: I3b5bf8a5f62dcdf267662bb97a2073150152cfa6
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -4,6 +4,7 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
 - lunch:    lunch <product_name>-<build_variant>
 - tapas:    tapas [<App1> <App2> ...] [arm|x86|mips] [eng|userdebug|user]
 - croot:    Changes directory to the top of the tree.
+- cout:     Changes directory to out.
 - groot:    Changes directory to the root of the git project.
 - m:        Makes from the top of the tree.
 - mm:       Builds all of the modules in the current directory.
@@ -825,6 +826,15 @@ function groot()
         cd $(git rev-parse --show-cdup)
     else
         echo "Already at the root of the git project."
+    fi
+}
+
+function cout()
+{
+    if [  "$OUT" ]; then
+        cd $OUT
+    else
+        echo "Couldn't locate out directory.  Try setting OUT."
     fi
 }
 
