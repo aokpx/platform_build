94575acf8cc4e900468911d2d9caad0b7a11534e
Rob Beane <robbeane@gmail.com>
2013-08-16 18:54:53 -0400

    core/combo: Remove --icf=safe from linker flags
    Change-Id: I702022b3525c960682211a024a89a6f13ba0d2cb

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -163,8 +163,6 @@ TARGET_GLOBAL_LDFLAGS += \
 			-Wl,-z,relro \
 			-Wl,-z,now \
 			-Wl,--warn-shared-textrel \
-			-Wl,--fatal-warnings \
-			-Wl,--icf=safe \
 			$(arch_variant_ldflags)
 
 TARGET_GLOBAL_CFLAGS += -mthumb-interwork
